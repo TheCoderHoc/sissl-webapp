2e2f916976c5ad790d3284ff270a15ca68be630d
Asukycode <asamachi540@gmail.com>
2025-08-07 06:03:41 +0100

    refactored HorizontalSlider

--- src/app/(website)/product/smart-security-product/page.tsx
+++ src/app/(website)/product/smart-security-product/page.tsx
@@ -2,42 +2,41 @@ import React from "react";
 
 import HeroSection from "../../../../modules/website/smart-security-product/HeroSection";
 import GallerySection from "../../../../modules/website/smart-security-product/GallerySection";
+import HorizontalSlider from "@/components/shared/HorizontalSlider";
+import { slide1 } from "@/public/images";
 
 // Carousel items
-// const items = [
-//     {
-//         image: slide1.src,
-//         title: "Corporates & Enterprises",
-//         text: "Keep permanent and contract staff verified and secure—across departments, branches, or regions.",
-//     },
-//     {
-//         image: slide1.src,
-//         title: "Government",
-//         text: "Maintain trusted identity verification for volunteers, staff, and citizens—digitally and consistently.",
-//     },
-//     {
-//         image: slide1.src,
-//         title: "Education",
-//         text: "Secure onboarding and identity verification for students, educators, and alumni across institutions.",
-//     },
-//     {
-//         image: slide1.src,
-//         title: "Healthcare",
-//         text: "Authenticate professionals and patients to improve service delivery and data access security.",
-//     },
-// ];
+const items = [
+    {
+        image: slide1.src,
+        title: "Corporates & Enterprises",
+        description: "Keep permanent and contract staff verified and secure—across departments, branches, or regions.",
+    },
+    {
+        image: slide1.src,
+        title: "Government",
+        description: "Maintain trusted identity verification for volunteers, staff, and citizens—digitally and consistently.",
+    },
+    {
+        image: slide1.src,
+        title: "Education",
+        description: "Secure onboarding and identity verification for students, educators, and alumni across institutions.",
+    },
+    {
+        image: slide1.src,
+        title: "Healthcare",
+        description: "Authenticate professionals and patients to improve service delivery and data access security.",
+    },
+];
 
 const page = () => {
     return (
         <div className="dark:bg-[#030302] bg-white">
             <HeroSection />
             <GallerySection />
-            <div className="top text-[40px] bg-white dark:bg-black text-black dark:text-white text-center mx-auto  pb-[177px]">
-                Industries That Use <span className="yText">SISSL</span>
-            </div>
-            <section className="bg-white dark:bg-black flex flex-col justify-between w-[100%] pl-[10%]  my-0 mx-auto py-24 relative">
-                {/* <HorizontalSlider items={items} /> */}
-            </section>
+                <HorizontalSlider heading="Industries That Use SISSL"
+                    highlight="SISSL"
+                    items={items} />
         </div>
     );
 };
